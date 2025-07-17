b46b8cc2deac71c4b8234c418853df1104f299ca
Martin <martin.galdeano@gmail.com>
2025-07-17 11:09:34 -0300

    Update superbaseClient.js

--- superbaseClient.js
+++ superbaseClient.js
@@ -4,6 +4,6 @@ import { createClient as create } from "https://cdn.jsdelivr.net/npm/@supabase/s
 export function createClient() {
   return create(
     "https://djvdjulfeuqohpnatdmt.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdmRqdWxmZXVxb2hwbmF0ZG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNTYyMTEsImV4cCI6MjA2NzkzMjIxMX0.aD_klISxuwoGxMYtPq6XPARva8D32YkKFu7s_Ffq0fw"
   );
 }
