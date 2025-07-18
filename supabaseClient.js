064d001ec166ed5b106a153d1c96b1c24236096d
Martin <martin.galdeano@gmail.com>
2025-07-18 00:47:02 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,11 @@
 // supabaseClient.js
-import { createClient as create } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export function createClient() {
-  return create(
-    "https://djvdjulfeuqohpnatdmt.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdmRqdWxmZXVxb2hwbmF0ZG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNTYyMTEsImV4cCI6MjA2NzkzMjIxMX0.aD_klISxuwoGxMYtPq6XPARva8D32YkKFu7s_Ffq0fw"
-  );
+const supabaseUrl = "https://djvdjulfeuqohpnatdmt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdmRqdWxmZXVxb2hwbmF0ZG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNTYyMTEsImV4cCI6MjA2NzkzMjIxMX0.aD_klISxuwoGxMYtPq6XPARva8D32YkKFu7s_Ffq0fw";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export function createClientInstance() {
+  return supabase;
 }
