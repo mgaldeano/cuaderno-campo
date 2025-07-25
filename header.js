5322b749a8b7b1c0724d0e719d6369cff3d736e4
Martin <martin.galdeano@gmail.com>
2025-07-25 02:55:38 -0300

    Update header.js

--- header.js
+++ header.js
@@ -1,8 +1,4 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const supabase = createClient(
-  "https://djvdjulfeuqohpnatdmt.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqdmRqdWxmZXVxb2hwbmF0ZG10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzNTYyMTEsImV4cCI6MjA2NzkzMjIxMX0.aD_klISxuwoGxMYtPq6XPARva8D32YkKFu7s_Ffq0fw"
-);
+import { supabase } from "./supabaseClient.js";
 
 console.log("Cuaderno de Campo - Header v1.0.0 cargado");
 
